9d3b326d8daceceb28e108f71ab0386207ae68d2
Vadim Kondra <kondra@mego.travel>
2026-03-08 23:20:45 +0300

    = handler and main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"io"
+	"go-yandex-practicum/internal/handler"
 	"net/http"
-	"strconv"
-
-	"github.com/go-chi/chi/v5"
 )
 
 type MemStorage struct {
@@ -15,48 +12,9 @@ type MemStorage struct {
 
 func main() {
 
-	r := chi.NewRouter()
-
-	r.Get("/", func(rw http.ResponseWriter, r *http.Request) {
-		io.WriteString(rw, "Hello, world!")
-	})
-
-	r.Route("/update", func(r chi.Router) {
-		r.Route("/{metric-type}", func(r chi.Router) {
-			r.Route("/{metric-name}", func(r chi.Router) {
-				r.Post("/{metric-value}", metricHandler)
-			})
-		})
-	})
+	r := handler.ConfigServerRouter()
 
 	// r передаётся как http.Handler
 	http.ListenAndServe(":8080", r)
 
-}
-
-func metricHandler(rw http.ResponseWriter, r *http.Request) {
-	// тут работа с метрикой
-	metricType := chi.URLParam(r, "metric-type")
-	//metricName := chi.URLParam(r, "metric-name")
-	metricValue := chi.URLParam(r, "metric-value")
-
-	switch metricType {
-	case "counter":
-		_, err := strconv.ParseInt(metricValue, 10, 64)
-		if err != nil {
-			http.Error(rw, "invalid counter value", http.StatusBadRequest)
-			return
-		}
-
-	case "gauge":
-		_, err := strconv.ParseFloat(metricValue, 64)
-		if err != nil {
-			http.Error(rw, "invalid gauge value", http.StatusBadRequest)
-			return
-		}
-
-	default:
-		http.Error(rw, "unknown metric type", http.StatusBadRequest)
-		return
-	}
 }
