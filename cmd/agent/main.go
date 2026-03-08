459c42e8f1da9d627152e6dc48ed348a25286b81
Vadim Kondra <kondra@mego.travel>
2026-03-08 23:19:43 +0300

    add base http client logic

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+
+	serverHost := "http://localhost:8080"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, serverHost, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+}
