32162e6ee7f06f9dcfda5ad09cc2cf4d312b3c97
Vadim Kondra <kondra@mego.travel>
2026-03-08 22:35:43 +0300

    + metricHandler + MemStorage

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"io"
 	"net/http"
-
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
+
+type MemStorage struct {
+	gauges   map[string]float64
+	counters map[string]int64
+}
 
 func main() {
 
@@ -20,32 +24,7 @@ func main() {
 	r.Route("/update", func(r chi.Router) {
 		r.Route("/{metric-type}", func(r chi.Router) {
 			r.Route("/{metric-name}", func(r chi.Router) {
-				r.Post("/{metric-value}", func(rw http.ResponseWriter, r *http.Request) {
-					// тут работа с метрикой
-					metricType := chi.URLParam(r, "metric-type")
-					//metricName := chi.URLParam(r, "metric-name")
-					metricValue := chi.URLParam(r, "metric-value")
-
-					switch metricType {
-					case "counter":
-						_, err := strconv.ParseInt(metricValue, 10, 64)
-						if err != nil {
-							http.Error(rw, "invalid counter value", http.StatusBadRequest)
-							return
-						}
-
-					case "gauge":
-						_, err := strconv.ParseFloat(metricValue, 64)
-						if err != nil {
-							http.Error(rw, "invalid gauge value", http.StatusBadRequest)
-							return
-						}
-
-					default:
-						http.Error(rw, "unknown metric type", http.StatusBadRequest)
-						return
-					}
-				})
+				r.Post("/{metric-value}", metricHandler)
 			})
 		})
 	})
@@ -53,4 +32,31 @@ func main() {
 	// r передаётся как http.Handler
 	http.ListenAndServe(":8080", r)
 
+}
+
+func metricHandler(rw http.ResponseWriter, r *http.Request) {
+	// тут работа с метрикой
+	metricType := chi.URLParam(r, "metric-type")
+	//metricName := chi.URLParam(r, "metric-name")
+	metricValue := chi.URLParam(r, "metric-value")
+
+	switch metricType {
+	case "counter":
+		_, err := strconv.ParseInt(metricValue, 10, 64)
+		if err != nil {
+			http.Error(rw, "invalid counter value", http.StatusBadRequest)
+			return
+		}
+
+	case "gauge":
+		_, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			http.Error(rw, "invalid gauge value", http.StatusBadRequest)
+			return
+		}
+
+	default:
+		http.Error(rw, "unknown metric type", http.StatusBadRequest)
+		return
+	}
 }
