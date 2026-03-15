77a5bf954a645d6068e3207f3353ae7e16f26da0
Vadim Kondra <kondra@mego.travel>
2026-03-15 14:38:55 +0300

    + struct MemStorage + interface MetricsStorage + implementation methods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	models "go-yandex-practicum/internal/model"
 	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
-
-type MemStorage struct {
-	gauges   map[string]float64
-	counters map[string]int64
-}
 
 func main() {
 
@@ -47,14 +43,14 @@ func metricHandler(rw http.ResponseWriter, r *http.Request) {
 	metricValue := chi.URLParam(r, "metric-value")
 
 	switch metricType {
-	case "counter":
+	case models.Counter:
 		_, err := strconv.ParseInt(metricValue, 10, 64)
 		if err != nil {
 			http.Error(rw, "invalid counter value", http.StatusBadRequest)
 			return
 		}
 
-	case "gauge":
+	case models.Gauge:
 		val, err := strconv.ParseFloat(metricValue, 64)
 		if err != nil {
 			http.Error(rw, "invalid gauge value", http.StatusBadRequest)
