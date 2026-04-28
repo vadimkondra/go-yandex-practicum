f0610bf503e8e21d867a2e1f58f64d23e09460c0
Vadim Kondra <kondra@mego.travel>
2026-04-29 01:30:45 +0300

    + migrations + docker-compose.yml

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module go-yandex-practicum
 
-go 1.25.0
+go 1.25.7
 
 require (
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/jackc/pgx/v5 v5.9.2
+	github.com/pressly/goose/v3 v3.27.1
 	go.uber.org/zap v1.27.1
 )
 
@@ -12,7 +13,9 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 )
--- internal/store/postgre_storage.go
+++ internal/store/postgre_storage.go
@@ -2,15 +2,25 @@ package store
 
 import (
 	"database/sql"
+	"embed"
+	"fmt"
+	"log"
+
+	"go-yandex-practicum/internal/model"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 )
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
 
 type PostgresStorage struct {
 	db *sql.DB
 }
 
 func NewPostgresStorage(databaseDSN string) (*PostgresStorage, error) {
+	log.Printf("DATABASE_DSN = %q", databaseDSN)
 	db, err := sql.Open("pgx", databaseDSN)
 	if err != nil {
 		return nil, err
@@ -36,137 +46,138 @@ func (s *PostgresStorage) Close() error {
 }
 
 func (s *PostgresStorage) createTables() error {
-	/*_, err := s.db.Exec(`
-		CREATE TABLE IF NOT EXISTS metrics (
-			id TEXT PRIMARY KEY,
-			type TEXT NOT NULL,
-			delta BIGINT,
-			value DOUBLE PRECISION
-		)
-	`)*/
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("set goose dialect: %w", err)
+	}
+
+	if err := goose.Up(s.db, "migrations"); err != nil {
+		return fmt.Errorf("run postgres migrations: %w", err)
+	}
+
 	return nil
 }
 
 func (s *PostgresStorage) SetGauge(name string, value float64) error {
-
-	/*_, err := s.db.Exec(`
+	_, err := s.db.Exec(`
 		INSERT INTO metrics (id, type, value, delta)
 		VALUES ($1, $2, $3, NULL)
 		ON CONFLICT (id) DO UPDATE
 		SET type = EXCLUDED.type,
 		    value = EXCLUDED.value,
 		    delta = NULL
-	`, name, model.Gauge, value)
-	return err*/
-	return nil
+	`, name, models.Gauge, value)
+
+	return err
 }
 
 func (s *PostgresStorage) AddCounter(name string, delta int64) (int64, error) {
+	var result int64
 
-	/*var result int64
 	err := s.db.QueryRow(`
 		INSERT INTO metrics (id, type, delta, value)
 		VALUES ($1, $2, $3, NULL)
 		ON CONFLICT (id) DO UPDATE
 		SET type = EXCLUDED.type,
-		    delta = metrics.delta + EXCLUDED.delta,
+		    delta = COALESCE(metrics.delta, 0) + EXCLUDED.delta,
 		    value = NULL
 		RETURNING delta
-	`, name, model.Counter, delta).Scan(&result)
-	return result, err*/
-	return 0, nil
+	`, name, models.Counter, delta).Scan(&result)
+
+	return result, err
 }
 
 func (s *PostgresStorage) GetGauge(name string) (float64, bool, error) {
+	var value float64
 
-	/*var value float64
 	err := s.db.QueryRow(`
 		SELECT value
 		FROM metrics
 		WHERE id = $1 AND type = $2
-	`, name, model.Gauge).Scan(&value)
+	`, name, models.Gauge).Scan(&value)
 	if err == sql.ErrNoRows {
 		return 0, false, nil
 	}
 	if err != nil {
 		return 0, false, err
 	}
+
 	return value, true, nil
-	*/
-	return 0, false, nil
 }
 
 func (s *PostgresStorage) GetCounter(name string) (int64, bool, error) {
+	var delta int64
 
-	/*var delta int64
 	err := s.db.QueryRow(`
 		SELECT delta
 		FROM metrics
 		WHERE id = $1 AND type = $2
-	`, name, model.Counter).Scan(&delta)
+	`, name, models.Counter).Scan(&delta)
 	if err == sql.ErrNoRows {
 		return 0, false, nil
 	}
 	if err != nil {
 		return 0, false, err
 	}
+
 	return delta, true, nil
-	*/
-	return 0, false, nil
 }
 
 func (s *PostgresStorage) GetAllGauges() (map[string]float64, error) {
-
-	/*rows, err := s.db.Query(`
+	rows, err := s.db.Query(`
 		SELECT id, value
 		FROM metrics
 		WHERE type = $1
-	`, model.Gauge)
+	`, models.Gauge)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	result := make(map[string]float64)
 	for rows.Next() {
 		var name string
 		var value float64
+
 		if err := rows.Scan(&name, &value); err != nil {
 			return nil, err
 		}
+
 		result[name] = value
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
-	return result, nil*/
 
-	return nil, nil
+	return result, nil
 }
 
 func (s *PostgresStorage) GetAllCounters() (map[string]int64, error) {
-
-	/*rows, err := s.db.Query(`
+	rows, err := s.db.Query(`
 		SELECT id, delta
 		FROM metrics
 		WHERE type = $1
-	`, model.Counter)
+	`, models.Counter)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	result := make(map[string]int64)
 	for rows.Next() {
 		var name string
 		var delta int64
+
 		if err := rows.Scan(&name, &delta); err != nil {
 			return nil, err
 		}
+
 		result[name] = delta
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
-	return result, nil*/
 
-	return nil, nil
+	return result, nil
 }
