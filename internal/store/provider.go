7a1240224c286c17c30350d555f0ef2898d0e489
Vadim Kondra <kondra@mego.travel>
2026-04-30 08:33:07 +0300

    ! after merge

--- internal/store/provider.go
+++ internal/store/provider.go
@@ -4,8 +4,8 @@ import "go-yandex-practicum/internal/config"
 
 func NewStorage(cfg config.ServerConfig) (Storage, error) {
 
-	if cfg.DatabaseDsn != "" {
-		return NewPostgresStorage(cfg.DatabaseDsn)
+	if cfg.DatabaseDSN != "" {
+		return NewPostgresStorage(cfg.DatabaseDSN)
 	}
 	if cfg.FileStorePath != "" {
 		return NewFileStorage(cfg.FileStorePath, cfg.StoreInterval, cfg.Restore)
